6e0b814a50992971308ccc6566a3211fcd8f2665
hrhpink <149098992+hrhpink@users.noreply.github.com>
2025-07-21 22:39:01 -0400

    Added a . before base-- this successfully deploys the app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/hrhpink.github.io/",
+  base:"./hrhpink.github.io/",
 })
